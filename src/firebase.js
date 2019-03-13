851f9ff52b5e88c0f94bd99bfd6821d63fb67e73
Emily Creamer <emily.creamer@icloud.com>
2019-03-13 16:17:42 -0700

    Created function to load login UI and redirect

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const config = {
+    apiKey: "AIzaSyCGgezVBbMTimo0rKwccTp_LhP7T_-4718",
+    authDomain: "weatherwebapp-2d13f.firebaseapp.com"
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
--- test/header-component.test.js
+++ test/header-component.test.js
@@ -1,20 +1,7 @@
+import makeHeader from '../src/make-header.js';
+
 const test = QUnit.test;
 QUnit.module('Create header test');
-
-function makeHeader() {
-    const html = /*html*/ `
-    <header>
-        <div id="header-container">
-            <img id="logo" src="assets/weather-icon.png" alt="weather logo with sun, cloud, lightning, and raindrops">
-            <h1>City Weather Search</h1>
-        </div>
-    </header>
-    `;
-
-    const template = document.createElement('template');
-    template.innerHTML = html;
-    return template.content;
-}
 
 test('create header template', assert => {
     //arrange
