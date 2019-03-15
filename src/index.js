40a5e9afe89662c7709ac656899b70d067c1233f
Emily Creamer <emily.creamer@icloud.com>
2019-03-15 09:25:41 -0700

    Fetch URL working with API

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import loadHeader from './header-component.js';
 import writeSearchToQuery, { readFromQuery } from './hash-query.js';
 import './search-component.js';
 import updateQ from './search-component.js';
+import makeSearchUrl from './make-url.js';
 
 loadHeader();
 
@@ -9,4 +10,13 @@ window.addEventListener('hashchange', () => {
     const q = window.location.hash.slice(1);
     const qOptions = readFromQuery(q);
     updateQ(qOptions.q);
+    
+    const url = makeSearchUrl(qOptions);
+    
+    fetch(url)
+        .then(res => res.json())
+        .then(response => {
+        console.log(response);
+        }
+    )
 });
